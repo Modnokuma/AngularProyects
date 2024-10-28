b626ea5890049b44446f03e81735bbb013640f6c
Daniel Martinez <danmc018@gmail.com>
2024-10-28 13:33:46 +0100

    Lecciones 138 - 142

--- 06 - base/src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
+++ 06 - base/src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
@@ -1,5 +1,6 @@
+import { Ticket } from './../ticket.model';
 import { ButtonComponent } from './../../../shared/button/button.component';
-import { AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, output, Output, viewChild, ViewChild } from '@angular/core';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
 
@@ -17,26 +18,32 @@ export class NewTicketComponent implements AfterViewInit, OnInit {
     //   console.log("Entered Title: " + enteredTitle);
     //   console.dir(titleInput);
     // }
-    @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
 
+    @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
     // Con señal (necesario Angular v17.3)
     //private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+    //@Output() add = new EventEmitter<{title: string, text:string}>();
+    add = output<{title: string, text:string}>();
 
     ngOnInit(): void {
       console.log("ONINIT");
       console.log(this.form?.nativeElement);
     }
 
-    //garantiza tener acceso a los elementos de viewchild()
+    //Garantiza tener acceso a los elementos de viewchild()
     ngAfterViewInit() {
       console.log("AFTER VIEW INIT");
       console.log(this.form?.nativeElement);
     }
+    
     onSubmit(titleInput: string, ticketText: string){
       console.log("Entered Title: " + titleInput);
       console.log("Entered Text: " + ticketText);
+
+      this.add.emit({title: titleInput, text: ticketText});
     
-      this.form?.nativeElement.reset();
-      
+      this.form?.nativeElement.reset();      
     }
+
 }
